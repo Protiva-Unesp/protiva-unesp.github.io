502ddab7619ed94c7f8fafd3fb96432f2f31f948
fessouza-23 <joao.fessouza@gmail.com>
2024-10-23 15:54:57 -0300

    Add imagens e alterar textos das seções

--- src/components/sections/Sections.tsx
+++ src/components/sections/Sections.tsx
@@ -15,10 +15,10 @@ function Section() {
                             Com nosso novo site, o aprendizado ficou 
                             ainda mais completo!</p>
                     </div>
-                    <div className={styles.imgContent}>
-                        <h3>Imagem Protiva</h3>                 
-                        <h3>Imagem DCO (Dep. Computação)</h3>                 
-                        <h3>Imagem Unesp</h3>                 
+                    <div className={styles.imgContentGrid}>
+                        <img style={{ filter: 'saturate(5)'}} className="grid-item" src="./src/assets/img/logo_dco.png" alt="dco" />
+                        <img className="grid-item" src="./src/assets/img/logo-bgwhite.png" alt="dco" />                 
+                        <img className="grid-item" src="./src/assets/img/logo_unesp.svg" alt="dco" />                
                     </div>
                 </div>
             </div>
@@ -26,19 +26,17 @@ function Section() {
                 <div className={styles.sectionContent}>
                     <div className={styles.textContent}>
                         <h3 id="sectionEda">Estruturas de Dados e Algoritmos</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
-                        elit. Vivamus dictum tempor diam, quis consectetur mi 
-                        pulvinar vel. Integer id felis diam. Quisque aliquet, 
-                        justo quis scelerisque viverra, nunc justo cursus 
-                        tortor, vel ultricies urna eros in elit. Pellentesque 
-                        magna tellus, convallis sit amet rhoncus sit amet, 
-                        pulvinar sed odio.</p>
+                        <p>As estruturas de dados são ferramentas essenciais para 
+                            organizar os dados de forma eficiente, otimizar algoritmos 
+                            e desenvolver soluções mais elegantes para problemas complexos.
+                            Aqui você vai entender quais são as estruturas de dados existentes,
+                            qual situação usar cada tipo de estrutura, suas abordagens e aplicações.</p>
                         <Link to="/estude/eda">
                             <button>Aprender ED&A</button>
                         </Link>
                     </div>
                     <div className={styles.imgContent}>
-                        <h3>Imagem Grafos</h3>                 
+                        <img src="/src/assets/img/img_grap.png" alt="grafo" />
                     </div>
                 </div>
             </div>
@@ -46,19 +44,16 @@ function Section() {
                 <div className={styles.sectionContent}>
                     <div className={styles.textContent}>
                         <h3 id="sectionObi">OBI - Respostas comentadas</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
-                        elit. Vivamus dictum tempor diam, quis consectetur mi 
-                        pulvinar vel. Integer id felis diam. Quisque aliquet, 
-                        justo quis scelerisque viverra, nunc justo cursus 
-                        tortor, vel ultricies urna eros in elit. Pellentesque 
-                        magna tellus, convallis sit amet rhoncus sit amet, 
-                        pulvinar sed odio.</p>
+                        <p>A Olimpíada Brasileira de Informática (OBI) é uma competição nacional 
+                            que estimula o raciocínio lógico e a habilidade em programação. 
+                            Sabemos que entender a solução de um problema é essencial para aprimorar suas habilidades. 
+                            Por isso, disponibilizamos resoluções de questões anteriores da OBI.</p>
                         <Link to="/estude/obi">
                             <button>Estudar Respostas</button>
                         </Link>
                     </div>
                     <div className={styles.imgContent}>
-                        <h3>Imagem OBI</h3>                 
+                        <img src="/src/assets/img/logo_obi.svg" alt="grafo" />                
                     </div>
                 </div>
             </div>
@@ -66,19 +61,21 @@ function Section() {
                 <div className={styles.sectionContent}>
                     <div className={styles.textContent}>
                         <h3 id="sectionIntro">Introdução à Programação</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
-                        elit. Vivamus dictum tempor diam, quis consectetur mi 
-                        pulvinar vel. Integer id felis diam. Quisque aliquet, 
-                        justo quis scelerisque viverra, nunc justo cursus 
-                        tortor, vel ultricies urna eros in elit. Pellentesque 
-                        magna tellus, convallis sit amet rhoncus sit amet, 
-                        pulvinar sed odio.</p>
+                        <p>Aqui você dá seus primeiros passos para se tornar um programador e 
+                            solucionar problemas por meio de software. Você desenvolverá a 
+                            habilidade de criar algoritmos simples e entenderá como 
+                            transformar ideias em código.</p>
                         <Link to="/estude/intro">
                             <button>Iniciar</button>
                         </Link>
                     </div>
                     <div className={styles.imgContent}>
-                        <h3>Imagem Introdução</h3>                 
+                        <img
+                            style={{ maxWidth: '70%' }}
+                            className="grid-item"
+                            src="/src/assets/img/logo_program.svg"
+                            alt="programação"
+                        />
                     </div>
                 </div>
             </div>
