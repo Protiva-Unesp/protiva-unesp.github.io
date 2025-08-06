e9d082b867faaa7a2532b816f7211e8f6f5a5e41
txdtt <er.teixeira01@gmail.com>
2025-08-06 17:59:08 -0300

    Index google

---
+++ gatsby-ssr.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta
+      key="google-site-verification"
+      name="google-site-verification"
+      content="tM0N0sXv94lwFqiW0ewv1nLCbL5wQOuU8bZ7YlC70v4"
+    />,
+  ]);
+};
+
